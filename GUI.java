94a592929c1e274b430861d697a7474845d96f98
Clemens Zander <cle.zander@gmail.com>
2019-05-15 18:40:07 +0200

    Hauptmenü Klasse hinzugefügt

---
+++ GUI.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+/**
+ * Haupt-GUI des Hüpfburg-2D Spiels
+ *
+ * @author (Clemens Zander, Shium Rahman)
+ * @version (15.05.2019)
+ */
+public class GUI extends JFrame implements ActionListener
+{
+    private JButton start;
+    private JButton einstellungen;
+    private JButton info;
+    private JButton ende;
+    private Spielfeld spielfeld;
+    
+    public static void main(String[] args)
+    {
+        GUI frame = new GUI("Hauptmenü");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    
+    public GUI(String title)
+    {
+        super(title);
+        start = new JButton("Spiel Starten");
+        start.setBounds(120, 40, 160, 40);
+        add(start);
+        start.addActionListener(this);
+        
+        einstellungen = new JButton("Einstellungen");
+        einstellungen.setBounds(120, 120, 160, 40);
+        add(einstellungen);
+        einstellungen.addActionListener(this);
+        
+        info = new JButton("Spielanleitung");
+        info.setBounds(120, 200, 160, 40);
+        add(info);
+        info.addActionListener(this);
+        
+        ende = new JButton("Beenden");
+        ende.setBounds(120, 280, 160, 40);
+        add(ende);
+        ende.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==start)
+        {
+            spielfeld = new Spielfeld("Hüpfburg-2D",800,600);
+        }
+        
+        if(e.getSource()==einstellungen)
+        {
+            //ToDo:
+        }
+        
+        if(e.getSource()==info)
+        {
+            //ToDo:
+        }
+        
+        if(e.getSource()==ende)
+        {
+            System.exit(0);
+        }
+    }
+}
--- Spieler.java
+++ Spieler.java
@@ -13,33 +13,33 @@ public class Spieler extends Sprite
         super(i, x, y, delay, spielfeld);
     }
     
-    // @Override
-    // public void doLogic(long delta)
-    // {
-        // super.doLogic(delta);
+    @Override
+    public void doLogic(long delta)
+    {
+        super.doLogic(delta);
         
-        // if(getX()<0)
-        // {
-            // setHorizontalSpeed(0);
-            // setX(0);
-        // }
+        if(getX()<0)
+        {
+            setHorizontalSpeed(0);
+            setX(0);
+        }
         
-        // if(getX()+getWidth()>parent.getWidth())
-        // {
-            // setX(parent.getWidth()-getWidth());
-            // setHorizontalSpeed(0);
-        // }
+        if(getX()+getWidth()>parent.getWidth())
+        {
+            setX(parent.getWidth()-getWidth());
+            setHorizontalSpeed(0);
+        }
         
-        // if(getY<0)
-        // {
-            // setY(0);
-            // setVerticalSpeed(0);
-        // }
+        if(getY()<0)
+        {
+            setY(0);
+            setVerticalSpeed(0);
+        }
         
-        // if(getY()+getHeight()>parent.getHeight())
-        // {
-            // setY(parent.getHeight()-getHeight());
-            // serVerticalSpeed(0);
-        // }
-    // }
+        if(getY()+getHeight()>parent.getHeight())
+        {
+            setY(parent.getHeight()-getHeight());
+            setVerticalSpeed(0);
+        }
+    }
 }
